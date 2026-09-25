f1e5602de93854c7653f4e438a41456cc6d6735b
Joey574 <joeysoroka@outlook.com>
2026-09-24 22:28:50 -0700

    fix test script shebang, add vendor hash, and add empty body to engine functions

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -14,11 +14,17 @@ func New() *Engine {
 // information from other instances before using costly bandwidth
 //
 // ReadContext can return an error if the information is not found or a connection can't be made
-func (e *Engine) ReadContext(ctx context.Context, req *ReadRequest) (*ReadResponse, error)
+// If no error is returned then ReadResponse contains a valid pointer to the response
+func (e *Engine) ReadContext(ctx context.Context, req *ReadRequest) (*ReadResponse, error) {
+	return nil, nil
+}
 
 // WriteContext takes a context which can be cancelled, forcing an early exit and a write request
 // WriteContext writes directly to the rover, as this function is meant to send information which will change
 // the state of the rover
 //
 // WriteContext can return an error if the connection can't be made or the rover denies the request
-func (e *Engine) WriteContext(ctx context.Context, req *WriteRequest) (*WriteResponse, error)
+// If no error is returned then WriteResponse contains a valid pointer to the response
+func (e *Engine) WriteContext(ctx context.Context, req *WriteRequest) (*WriteResponse, error) {
+	return nil, nil
+}
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/bin/env sh
+#!/bin/sh
 
 mkdir -p /var/tmp/nixos-vm 2>/dev/null
 XDG_RUNTIME_DIR=/var/tmp/nixos-vm ./result/bin/nixos-test-driver
